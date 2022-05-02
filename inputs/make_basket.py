4e1d878ad2ff37f0dcd5fccf1beb57ea9186ee82
Justin Gaudet <gaudet@gmail.com>
2022-05-02 19:03:03 -0300

    restructured input and add "free above" handling

--- inputs/make_basket.py
+++ inputs/make_basket.py
@@ -2,106 +2,153 @@ import csv
 import json
 
 items_file = "inputs/items.json"
-stores_file = "inputs/stores.csv"
-
-items_file1 = "inputs/items1.json"
-stores_file1 = "inputs/stores1.csv"
 
 ITEMS = None
 STORES = None
+STORE_HAS_ITEMS = None
 
-
-def get_stores():
-    global STORES
-    with open(stores_file, "r") as csvfile:
-        STORES = [store for store in csv.DictReader(csvfile)]
-
-
-def get_items():
-    global ITEMS
-    with open(items_file, "r") as jsonfile:
-        ITEMS = json.load(jsonfile)
-
-
-def store_stores():
-    with open(stores_file1, "w") as csvfile:
-        writer = csv.DictWriter(csvfile, STORES[0])
-        writer.writeheader()
-        writer.writerows(STORES)
-
-
-def store_items():
-    with open(items_file1, "w") as jsonfile:
-        json.dump(ITEMS, jsonfile)
-
+# todo: add delete store (from item)
+# todo: add delete item
+# todo: add add item
 
 def init():
-    get_stores()
-    get_items()
+    global ITEMS
+    global STORES
+    global STORE_HAS_ITEMS
+    file_obj = None
+    with open(items_file, "r") as jsonfile:
+        file_obj = json.load(jsonfile)
+    ITEMS = file_obj["items"]
+    STORES = file_obj["stores"]
+    STORE_HAS_ITEMS = file_obj["storeHasItem"]
 
 
 shipping_types = [
     {"type": "free", "name": "free"},
     {
-        "type": "fixed",
-        "name": "fixed flat rate",
+        "type": "flat",
+        "name": "flat rate",
+        "variable": "price",
         "variable_name": "flat rate shipping cost",
     },
     {
-        "type": "flat rate",
+        "type": "fixed",
         "name": "fixed rate per item",
-        "variable_name": "flat rate shipping cost",
+        "variable": "price",
+        "variable_name": "shipping cost per item",
     },
-    {"type": "free above", "name": "free above", "variable_name": "minimum total?"},
+    {
+        "type": "free above",
+        "name": "free above",
+        "variable": "minimum_price",
+        "variable_name": "minimum total?",
+        "nested_type": "other_type",
+    },
 ]
 
 
 def add_new_store():
+    first_level_nesting_types = ["free above"]
     store_name = input("What is the new store name? > ")
-    store = {"ID": store_name, "Name": store_name}
+    store = {"name": store_name, "shipping": {}}
+    shipping_type_dict = store["shipping"]
     shipping_var_int = None
-    while not shipping_var_int:
-        print("{:<6} {}".format("", "SHIPPING TYPE"))
-        for index, shipping in enumerate(shipping_types):
-            print("{:<6} {}".format(index + 1, shipping["name"]))
-
-        shipping_type_in = input("Select a shipping type for the store? > ")
-        if shipping_type_in.isdigit():
-            shipping_var_int = int(shipping_type_in)
-            store["shipping"] = shipping_types[shipping_var_int - 1]["type"]
-        else:
-            print("That is not a valid selection")
-    if shipping_types[shipping_var_int - 1].get("variable_name"):
-        valid_number = False
-        while not valid_number:
-            shipping_var_in = input(
-                f"{shipping_types[shipping_var_int-1]['variable_name']} > "
+    is_nested = True
+    nested_level = -1
+    while is_nested:
+        is_nested = False
+        nested_level += 1
+        while not shipping_var_int:
+            current_shipping_types = [
+                shipping_type
+                for shipping_type in shipping_types
+                if shipping_type["type"] not in first_level_nesting_types
+                or nested_level == 0
+            ]
+            print(
+                "{:<6} {}".format(
+                    "", "SHIPPING TYPE" if nested_level == 0 else "NESTED SHIPPING TYPE"
+                )
             )
-            if shipping_var_in.isdigit():
-                shipping_var_int = int(shipping_var_in)
-                store["shipping_variable"] = shipping_var_int
-                valid_number = True
-            else:
-                print("That is not a valid number")
+            for index, shipping in enumerate(current_shipping_types):
+                print("{:<6} {}".format(index + 1, shipping["name"]))
 
-    base = (" {:<30}" * 2) + " {:<8}"
+            shipping_type_in = input("Select a shipping type for the store? > ")
+            if shipping_type_in.isdigit():
+                shipping_var_int = int(shipping_type_in)
+                if shipping_var_int < 1 or shipping_var_int > len(
+                    current_shipping_types
+                ):
+                    print("That shipping option does not exist")
+                    continue
+                shipping_type_dict['type'] = current_shipping_types[shipping_var_int - 1]["type"]
+            else:
+                print("That is not a valid selection")
+
+        shipping_type = current_shipping_types[shipping_var_int - 1]
+        if shipping_type.get("variable_name"):
+            valid_number = False
+            while not valid_number:
+                shipping_var_in = input(f"{shipping_type['variable_name']} > ")
+                if shipping_var_in.isdigit():
+                    shipping_var_int = int(shipping_var_in)
+                    shipping_type_dict[shipping_type["variable"]] = shipping_var_int
+                    valid_number = True
+                else:
+                    print("That is not a valid number")
+
+        nested_type = shipping_type.get("nested_type")
+        if nested_type is not None:
+            shipping_type_dict[nested_type] = {}
+            shipping_type_dict = shipping_type_dict[nested_type]
+            shipping_var_int = None
+            is_nested = True
+
+    base = (" {:<30}" * 2) + " {:<8} {:<30} {:<8}"
+    shipping_type = next(shipping_type_ for shipping_type_ in shipping_types if shipping_type_['type'] == store["shipping"]["type"])
     valid_input = False
     while not valid_input:
         print(
             base.format(
                 "NAME",
                 "SHIPPING TYPE",
-                "COST" if store.get("shipping_variable") else "",
+                "COST"
+                if not store["shipping"].get(shipping_type.get("variable")) is None
+                else "",
+                "ALTERNATE SHIPPING TYPE"
+                if not store["shipping"].get(shipping_type.get("nested_type")) is None
+                else "",
+                "ALTERNATE SHIPPING COST"
+                if not store["shipping"].get(shipping_type.get("nested_type")) is None
+                and not store["shipping"][shipping_type["nested_type"]].get(
+                    next(shipping_type_.get("variable") for shipping_type_ in shipping_types if shipping_type_['type'] == store["shipping"][shipping_type["nested_type"]]["type"])
+                )
+                is None
+                else "",
             )
         )
         print(
             base.format(
-                store["Name"], store["shipping"], store.get("shipping_variable") or ""
+                store["name"],
+                store["shipping"]["type"],
+                store["shipping"].get(shipping_type.get("variable")) or "",
+                store["shipping"][shipping_type["nested_type"]]["type"]
+                if not store["shipping"].get(shipping_type.get("nested_type")) is None
+                else "",
+                store["shipping"][shipping_type["nested_type"]][
+                    next(shipping_type_["variable"] for shipping_type_ in shipping_types if shipping_type_['type'] == store["shipping"][shipping_type["nested_type"]]["type"])
+                ]
+                if not store["shipping"].get(shipping_type.get("nested_type")) is None
+                and not store["shipping"][shipping_type["nested_type"]].get(
+                    next(shipping_type_.get("variable") for shipping_type_ in shipping_types if shipping_type_['type'] == store["shipping"][shipping_type["nested_type"]]["type"])
+                )
+                is None
+                else "",
             )
         )
         val_in = input(
             f"'a' to accept or 'c' to cancel and return to previous menu > "
-        ).tolower()
+        ).lower()
         if val_in == "a":
             STORES.append(store)
             valid_input = True
@@ -111,14 +158,14 @@ def add_new_store():
             print("That is not a valid selection")
 
 
-def add_store(stores):
+def add_store(stores_with_item, item_name):
     in_val = None
     while in_val != "r":
         print("select a store to add")
         print("{:<6} {}".format("", "NAME"))
-        store_names = {store["store id"] for store in stores}
+        store_names = {store["store"] for store in stores_with_item}
         remaining_stores = [
-            store_["Name"] for store_ in STORES if store_["ID"] not in store_names
+            store_["name"] for store_ in STORES if store_["name"] not in store_names
         ]
         for index, store in enumerate(remaining_stores):
             print("{:<6} {}".format(index + 1, store))
@@ -131,19 +178,15 @@ def add_store(stores):
                 continue
             valid_price = False
             while not valid_price:
-                store = {
-                    "store id": next(
-                        (
-                            store_["ID"]
-                            for store_ in STORES
-                            if store_["Name"] == remaining_stores[int_val - 1]
-                        )
-                    )
+                store_with_item = {
+                    "store": remaining_stores[int_val - 1],
+                    "item": item_name,
                 }
-                stores.append(store)
                 new_price = input("What is the price at the store? > ")
                 if new_price.isdigit():
-                    store["price"] = new_price
+                    store_with_item["price"] = new_price
+                    stores_with_item.append(store_with_item)
+                    STORE_HAS_ITEMS.append(store_with_item)
                     valid_price = True
                 elif new_price == "r":
                     break
@@ -158,45 +201,62 @@ def add_store(stores):
 def edit_item(item):
     in_val = None
     while in_val != "r":
-        stores = item["stores"]
         item_name = item["name"]
+        stores_with_item = [
+            {"original index": index, **value}
+            for index, value in enumerate(STORE_HAS_ITEMS)
+            if value["item"] == item_name
+        ]
         print(f"EDIT ITEM {item_name}")
         print("select an store number to edit store price")
         print("{:<6} {:<20} {}".format("", "NAME", "PRICE"))
-        for index, store in enumerate(stores):
-            print(
-                "{:<6} {:<20} {}".format(index + 1, store["store id"], store["price"])
-            )
+        for index, store in enumerate(stores_with_item):
+            print("{:<6} {:<20} {}".format(index + 1, store["store"], store["price"]))
         print(
             "\n'e' to edit item name\n'a' to add a store\n'r' to return to the previous menu"
         )
         in_val = input("> ").lower()
         if in_val.isdigit():
             int_val = int(in_val)
-            if int_val < 1 or int_val > len(ITEMS):
+            if int_val < 1 or int_val > len(stores_with_item):
                 print("That store does not exist")
                 continue
             valid_price = False
             while not valid_price:
                 new_price = input("What is the new price? > ")
                 if new_price.isdigit():
-                    stores[int_val - 1]["price"] = float(new_price)
+                    STORE_HAS_ITEMS[stores_with_item[int_val - 1]["original index"]][
+                        "price"
+                    ] = float(new_price)
                     valid_price = True
                 elif new_price == "r":
                     break
                 else:
                     print("That is not a valid price")
         if in_val == "e":
-            item["name"] = input("What is the new name? > ")
+            STORE_HAS_ITEMS[stores_with_item[int_val - 1]["original index"]][
+                "item"
+            ] = input("What is the new name? > ")
+            item = next(
+                (
+                    item
+                    for item in ITEMS
+                    if item["name"] == stores_with_item[int_val - 1]["item"]
+                )
+            )
+            item["name"] = STORE_HAS_ITEMS[
+                stores_with_item[int_val - 1]["original index"]
+            ]["item"]
         elif in_val == "a":
-            add_store(stores)
+            add_store(stores_with_item, item_name)
         elif in_val != "r":
             print("Invalid input")
 
 
 def save():
-    store_items()
-    store_stores()
+    file_obj = {"stores": STORES, "storeHasItem": STORE_HAS_ITEMS, "items": ITEMS}
+    with open(items_file, "w") as jsonfile:
+        json.dump(file_obj, jsonfile)
 
 
 def main():
@@ -221,7 +281,7 @@ def main():
         elif in_val == "s":
             save()
             in_val = "q"
-        else:
+        elif in_val != "q":
             print("Invalid input")
 
 
