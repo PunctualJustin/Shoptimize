c93eed869d5ec145ed1c12b3c1bd7bd2a61237e3
Justin Gaudet <gaudet@gmail.com>
2022-05-03 11:30:03 -0300

    refactor, add 'fixed rate' and 'free above' constraints

--- src/core/optimize/constraints.py
+++ src/core/optimize/constraints.py
@@ -9,8 +9,18 @@ MAX_STORE_ITEMS = 10000000001
 
 def get_constraints(stores):
     constraints = one_and_all_items(stores)
-    constraints.update(flat_rate_constraints(stores))
+    for store in stores.values():
+        make_shipping_constraints(constraints, store, store.shipping.type_)
     return constraints
+
+
+def make_shipping_constraints(constraints, store, shipping, modifier = 0):
+    if shipping == "flat":
+        flat_rate_constraint(constraints, store, modifier)
+    elif shipping == "fixed":
+        fixed_rate_constraint(constraints, store, modifier)
+    elif shipping == "free above":
+        free_above_constraint(constraints, store)
 
 
 def one_and_all_items(stores):
@@ -22,7 +32,7 @@ def one_and_all_items(stores):
             else:
                 items[item] = [store]
     constraints = {
-        f"oaai_{store.id}_{item}": LpConstraint(
+        f"oaai_{store.name}_{item}": LpConstraint(
             lpSum(store.lp_variables[f"{store.name}_{item}"] for store in stores), rhs=1
         )
         for item, stores in items.items()
@@ -30,20 +40,46 @@ def one_and_all_items(stores):
     return constraints
 
 
-def flat_rate_constraints(stores):
-    constraints = {
-        f"flat_rate_{store.id}": LpConstraint(
-            (
-                MAX_STORE_ITEMS * store.lp_variables[f"{store.name}_shipping"]
-                - lpSum(
-                    store.lp_variables[f"{store.id}_{item}"]
-                    for item in store.items
-                )
-            ),
-            const.LpConstraintGE,
-            rhs=0,
-        )
-        for store in stores.values()
-        if store.shipping.type_ == "flat"
-    }
-    return constraints
+def flat_rate_constraint(constraints, store, modifier):
+    constraints[f"flat_rate_{store.name}"] = LpConstraint(
+        (
+            MAX_STORE_ITEMS * store.lp_variables[f"{store.name}_shipping"]
+            - lpSum(store.lp_variables[f"{store.name}_{item}"] for item in store.items)
+            + MAX_STORE_ITEMS * modifier
+        ),
+        const.LpConstraintGE,
+        rhs=0,
+    )
+
+
+def fixed_rate_constraint(constraints, store, modifier):
+    constraints.update(
+        {
+            f"fixed_rate_{store.name}_{item}": LpConstraint(
+                (
+                    store.lp_variables[f"{store.name}_{item}"]
+                    - store.lp_variables[f"{store.name}_{item}_shipping"]
+                    - modifier
+                ),
+                rhs=0,
+            )
+            for item in store.items
+        }
+    )
+
+
+def free_above_constraint(constraints, store):
+    constraints[f"free_above_{store.name}"] = LpConstraint(
+        (
+            lpSum(store.lp_variables[f"{store.name}_{item_name}"]*price for item_name, price in store.items.items())
+            - store.lp_variables[f"{store.name}_shipping_minimum"]*store.shipping.minimum_price
+        ),
+        const.LpConstraintGE,
+        rhs=0,
+    )
+    constraints.update({f"free_above_{store.name}_{variable.name}": LpConstraint(
+        variable + store.lp_variables[f"{store.name}_shipping_minimum"],
+        const.LpConstraintLE,
+        rhs=1,
+    ) for variable in store.shipping.other_type.get_shipping_variables()})
+    make_shipping_constraints(constraints, store, store.shipping.other_type.type_, store.lp_variables[f"{store.name}_shipping_minimum"])
