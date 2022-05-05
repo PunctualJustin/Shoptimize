1cc618acd216e4a49380b840ec3b10173a04beaf
Justin Gaudet <gaudet@gmail.com>
2022-05-04 22:16:01 -0300

    add dynamic shipping

--- src/core/optimize/constraints.py
+++ src/core/optimize/constraints.py
@@ -1,6 +1,4 @@
-import sys
-from typing import OrderedDict
-from pulp import LpVariable, LpProblem, lpSum, LpConstraint, const
+from pulp import lpSum, LpConstraint, const
 
 # this should be greater than the number of items that will be added to a store. It is used in a number constraints
 #  where sys.maxsize*2+1 does not work correctly, nor does anything greater than this number
@@ -10,16 +8,18 @@ MAX_STORE_ITEMS = 10000000001
 def get_constraints(stores):
     constraints = one_and_all_items(stores)
     for store in stores.values():
-        make_shipping_constraints(constraints, store, store.shipping.type_)
+        make_shipping_constraints(constraints, store, store.shipping)
     return constraints
 
 
 def make_shipping_constraints(constraints, store, shipping, modifier=0):
-    if shipping == "flat":
+    if shipping.type_ == "flat":
         flat_rate_constraint(constraints, store, modifier)
-    elif shipping == "fixed":
+    elif shipping.type_ == "fixed":
         fixed_rate_constraint(constraints, store, modifier)
-    elif shipping == "free above":
+    elif shipping.type_ == "dynamic":
+        dynamic_rate_constraint(constraints, store, shipping, modifier)
+    elif shipping.type_ == "free above":
         free_above_constraint(constraints, store)
 
 
@@ -68,6 +68,47 @@ def fixed_rate_constraint(constraints, store, modifier):
     )
 
 
+def dynamic_rate_constraint(constraints, store, shipping, modifier):
+    constraints.update(
+        {
+            f"dynamic_{store.name}_{'_'.join(combo['items'])}": LpConstraint(
+                (
+                    lpSum(
+                        store.lp_variables[f"{store.name}_{item}"]
+                        for item in combo["items"]
+                    )
+                    - lpSum(
+                        store.lp_variables[
+                            f"{store.name}_{'_'.join(constraint_combo['items'])}_shipping"
+                        ]
+                        * len(
+                            list(
+                                filter(
+                                    lambda inner_item: inner_item in combo["items"],
+                                    constraint_combo["items"],
+                                )
+                            )
+                        )
+                        for constraint_combo in shipping.combinations
+                        if any(
+                            item in combo["items"] for item in constraint_combo["items"]
+                        )
+                    )
+                    - modifier * MAX_STORE_ITEMS
+                ),
+                const.LpConstraintLE,
+                rhs=0,
+            )
+            for combo in shipping.combinations
+        }
+    )
+    constraints[f"dynamic_{store.name}_control"] = LpConstraint(
+        lpSum(shipping.get_shipping_variables()) + modifier,
+        const.LpConstraintLE,
+        rhs=1,
+    )
+
+
 def free_above_constraint(constraints, store):
     constraints[f"free_above_{store.name}"] = LpConstraint(
         (
@@ -94,6 +135,6 @@ def free_above_constraint(constraints, store):
     make_shipping_constraints(
         constraints,
         store,
-        store.shipping.other_type.type_,
+        store.shipping.other_type,
         store.lp_variables[f"{store.name}_shipping_minimum"],
     )
--- src/core/store/shipping.py
+++ src/core/store/shipping.py
@@ -1,7 +1,4 @@
-import imp
-from pulp import LpVariable, lpSum
-
-from .variable_map import LpVariableMap
+from pulp import lpSum
 
 
 class Shipping:
@@ -9,26 +6,29 @@ class Shipping:
         lambda **kwargs: f"{kwargs['store']}_{kwargs['shipping_type_indicator']}"
     )
 
-    variable_types = {
+    variable_name_formats = {
         "free": {},
         "fixed": {
-            "item": lambda **kwargs: f"{kwargs['store']}_{kwargs['item']}_shipping"
+            "item": lambda *args, **kwargs: f"{kwargs['store']}_{kwargs['item']}_shipping"
         },
         "flat": {
-            "init": lambda **kwargs: Shipping.init_lp_variable(
+            "init": lambda *args, **kwargs: Shipping.init_lp_variable(
                 shipping_type_indicator="shipping", **kwargs
             )
         },
         "free above": {
-            "init": lambda **kwargs: Shipping.init_lp_variable(
+            "init": lambda *args, **kwargs: Shipping.init_lp_variable(
                 shipping_type_indicator="shipping_minimum", **kwargs
             )
+        },
+        "dynamic": {
+            "combo": lambda *args, **kwargs: f'{kwargs["store"]}_{"_".join(args)}_shipping'
         },
     }
 
     def __init__(self, store, **kwargs):
         self.store = store
-        if kwargs["type"] not in Shipping.variable_types:
+        if kwargs["type"] not in Shipping.variable_name_formats:
             raise Exception(f'unknown shipping type {kwargs["type"]}')
         self.type_ = kwargs["type"]
         self.lp_variables = self.store.lp_variables
@@ -38,36 +38,49 @@ class Shipping:
         elif self.type_ == "free above":
             self.minimum_price = kwargs["minimum_price"]
             self.other_type = Shipping(store, **kwargs["other_type"])
+        if self.type_ == "dynamic":
+            self.combinations = kwargs["combinations"]
+            self.add_combo()
 
-        self.lp_variables.add(self.get_lp_variable("init"))
+        self.lp_variables.add(self.get_lp_variable_name("init"))
 
     def __repr__(self) -> str:
         return self.type_
 
     def get_shipping_variables(self):
         if self.type_ in ["flat", "free_above"]:
-            return [self.lp_variables[self.get_lp_variable("init")]]
+            return [self.lp_variables[self.get_lp_variable_name("init")]]
         elif self.type_ == "fixed":
             return [
-                self.lp_variables[self.get_lp_variable("item", item=item)]
+                self.lp_variables[self.get_lp_variable_name("item", item=item)]
                 for item in self.store.items
+            ]
+        elif self.type_ == "dynamic":
+            return [
+                self.lp_variables[self.get_lp_variable_name("combo", *combo["items"])]
+                for combo in self.combinations
             ]
         return []
 
-    def get_lp_variable(self, variable_type, **kwargs):
-        return self.variable_types[self.type_].get(variable_type, lambda **x: None)(
-            store=self.store.name, **kwargs
-        )
+    def get_lp_variable_name(self, variable_type, *args, **kwargs):
+        return self.variable_name_formats[self.type_].get(
+            variable_type, lambda **x: None
+        )(*args, store=self.store.name, **kwargs)
 
     def add_item(self, item):
-        self.lp_variables.add(self.get_lp_variable("item", item=item))
+        self.lp_variables.add(self.get_lp_variable_name("item", item=item))
         if self.type_ == "free above":
             self.other_type.add_item(item)
+
+    def add_combo(self):
+        for combo in self.combinations:
+            self.lp_variables.add(self.get_lp_variable_name("combo", *combo["items"]))
 
     def get_item_objective(self, item):
         if self.type_ == "fixed":
             return (
-                self.lp_variables[self.get_lp_variable("item", item=item)] * self.price
+                self.lp_variables[self.get_lp_variable_name("item", item=item)]
+                * self.price
             )
         elif self.type_ == "free above":
             return self.other_type.get_item_objective(item)
@@ -75,7 +88,13 @@ class Shipping:
 
     def get_objective(self):
         if self.type_ == "flat":
-            return self.lp_variables[self.get_lp_variable("init")] * self.price
+            return self.lp_variables[self.get_lp_variable_name("init")] * self.price
         elif self.type_ == "free above":
             return self.other_type.get_objective()
+        elif self.type_ == "dynamic":
+            return lpSum(
+                self.lp_variables[self.get_lp_variable_name("combo", *combo["items"])]
+                * combo["price"]
+                for combo in self.combinations
+            )
         return 0
